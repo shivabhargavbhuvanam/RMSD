06393648e741ae5f9f6d98a018c1b2624d8ca8ed
Sai Siddhardha Chebrolu <144749543+chebrolusai@users.noreply.github.com>
2024-04-12 18:39:02 -0400

    removed remaining units

--- Retail_Management_System/Scripts/Inventory_clerk/inventory_clerk_script.sql
+++ Retail_Management_System/Scripts/Inventory_clerk/inventory_clerk_script.sql
@@ -5,7 +5,6 @@ BEGIN
     STORE_OWNER.ADD_PRODUCT(
         p_category => 'Clothing',
         p_name => 'Jeans',
-        p_remaining_units => 50,
         p_selling_price => 26.99
     );
 END;
@@ -14,7 +13,6 @@ BEGIN
     STORE_OWNER.ADD_PRODUCT(
         p_category => 'Kitchen',
         p_name => 'Pan',
-        p_remaining_units => 150,
         p_selling_price => 9.99
     );
 END;
@@ -23,7 +21,6 @@ BEGIN
     STORE_OWNER.ADD_PRODUCT(
         p_category => 'Furniture',
         p_name => 'Bed frame',
-        p_remaining_units => 50,
         p_selling_price => 50.99
     );
 END;
@@ -32,7 +29,6 @@ BEGIN
     STORE_OWNER.ADD_PRODUCT(
         p_category => 'Food',
         p_name => 'Lays',
-        p_remaining_units => 150,
         p_selling_price => 4.99
     );
 END;
