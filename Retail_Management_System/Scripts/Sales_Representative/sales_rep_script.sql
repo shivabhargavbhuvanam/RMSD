4328575fbe55df6e8fa2199926f545839f12c045
shivabhargavbhuvanam <145069226+shivabhargavbhuvanam@users.noreply.github.com>
2024-04-12 20:19:35 -0400

    Added more sales data

--- Retail_Management_System/Scripts/Sales_Representative/sales_rep_script.sql
+++ Retail_Management_System/Scripts/Sales_Representative/sales_rep_script.sql
@@ -65,37 +65,159 @@ END;
 
 DECLARE
     v_products STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
-        STORE_OWNER.product_type('Nike', 'Shoes', 4),
-        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 8),
+        STORE_OWNER.product_type('Nike', 'Shoes', 7),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 10),
         STORE_OWNER.product_type('Lays', 'Chips', 15),
         STORE_OWNER.product_type('Pan', 'Kitchen', 2)
     );
 BEGIN
-    STORE_OWNER.process_products(pi_products=>v_products, pi_customer_email=>'robert.brown@example.com', pi_employee_email=>'thomas@email.com');
+    STORE_OWNER.process_products(pi_products=>v_products, pi_customer_email=>'jimbean@email.com', pi_employee_email=>'thomas@email.com');
 END;
 /
 
 DECLARE
-    v_products STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+    v_products1 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
         STORE_OWNER.product_type('Nike', 'Shoes', 4),
         STORE_OWNER.product_type('Lays', 'Chips', 5),
-        STORE_OWNER.product_type('Pan', 'Kitchen', 4)
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 1)
     );
 BEGIN
-    STORE_OWNER.process_products(pi_products=>v_products, pi_customer_email=>'alice.j22@example.com', pi_employee_email=>'bobby@email.com');
+    STORE_OWNER.process_products(pi_products=>v_products1, pi_customer_email=>'jimbean@email.com', pi_employee_email=>'jane@email.com');
 END;
 /
 
 DECLARE
-    v_products STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+    v_products2 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
         STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
         STORE_OWNER.product_type('Jeans', 'Clothing', 5),
-        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
-        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
-        STORE_OWNER.product_type('Milk', 'Groceries', 4)
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3)
     );
 BEGIN
-    STORE_OWNER.process_products(pi_products=>v_products, pi_customer_email=>'emily.clark@example.com', pi_employee_email=>'bobby@email.com');
+    STORE_OWNER.process_products(pi_products=>v_products2, pi_customer_email=>'emily.clark@example.com', pi_employee_email=>'jonesrebecca@email.com');
+END;
+/
+
+DECLARE
+    v_products3 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('T-Shirt', 'Clothing', 5),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 2)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products3, pi_customer_email=>'alice.j22@example.com', pi_employee_email=>'bobby@email.com');
+END;
+/
+
+DECLARE
+    v_products4 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products4, pi_customer_email=>'robert.brown@example.com', pi_employee_email=>'carter@email.com');
+END;
+/
+
+DECLARE
+    v_products5 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products5, pi_customer_email=>'johndoe@email.com', pi_employee_email=>'bobby@email.com');
+END;
+/
+
+DECLARE
+    v_products6 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products6, pi_customer_email=>'alice.j22@example.com', pi_employee_email=>'carter@email.com');
+END;
+/
+
+DECLARE
+    v_products7 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products7, pi_customer_email=>'johndoe@email.com', pi_employee_email=>'bobby@email.com');
+END;
+/
+
+DECLARE
+    v_products8 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products8, pi_customer_email=>'robert.brown@example.com', pi_employee_email=>'thomas@email.com');
+END;
+/
+
+DECLARE
+    v_products9 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products9, pi_customer_email=>'alice.j22@example.com', pi_employee_email=>'thomas@email.com');
+END;
+/
+
+DECLARE
+    v_products10 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products10, pi_customer_email=>'robert.brown@example.com', pi_employee_email=>'thomas@email.com');
+END;
+/
+
+DECLARE
+    v_products11 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2),
+        STORE_OWNER.product_type('Milk', 'Groceries', 4),
+        STORE_OWNER.product_type('Jeans', 'Clothing', 5),
+        STORE_OWNER.product_type('Bed frame', 'Furniture', 3),
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products11, pi_customer_email=>'alice.j22@example.com', pi_employee_email=>'bobby@email.com');
+END;
+/
+
+DECLARE
+    v_products12 STORE_OWNER.product_list_type := STORE_OWNER.product_list_type(
+        STORE_OWNER.product_type('Retinoid', 'Cosmetics', 3),
+        STORE_OWNER.product_type('Smartphone', 'Electronics', 2)
+    );
+BEGIN
+    STORE_OWNER.process_products(pi_products=>v_products12, pi_customer_email=>'robert.brown@example.com', pi_employee_email=>'carter@email.com');
 END;
 /
 
